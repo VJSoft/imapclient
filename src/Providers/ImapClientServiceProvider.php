553442bed726ab7a5f15b5b6982709a4dfd49b57
jerry <jerry@vjsoft.net>
2018-04-10 11:35:29 +0300

    FIRST COMMIT

---
+++ src/Providers/ImapClientServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace vjsoft\imapclient\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ImapClientServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
